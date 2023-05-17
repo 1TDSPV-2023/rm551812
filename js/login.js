17dd2a9d7aa56fe56d0e49a4a0d24eb5914da621
kauezin05 <kauezinho_naurto@hotmail.com>
2023-05-16 22:59:21 -0300

    Verificador login + Olinho funcionando

--- js/login.js
+++ js/login.js
@@ -1,23 +1,46 @@
 "use strict"; //Serve para retringir algumas ações do coódigo
 
+
+// LISTA DE USUARIOS
+
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "JACINTO",
+        nomeUsuario : "JA",
+        senhaUsuario : "123"
+    },
+    {
+        nomeCompleto : "JACINTO2",
+        nomeUsuario : "JA2",
+        senhaUsuario : "123"
+    },
+    {
+        nomeCompleto : "JACINTO3",
+        nomeUsuario : "JA3",
+        senhaUsuario : "123"
+    }
+];
+
+localStorage.setItem( "listaUser", JSON.stringify( listaDeUsuarios));
+
 //CRIANDO OBJETOS
 
-const usuario1 = {
-    nomeUsu : "000",
-    senhaUsu : "0000"
+// const usuario1 = {
+//     nomeUsu : "000",
+//     senhaUsu : "0000"
 
-}
+// }
 
-const usuario2 = {
-    nomeUsu : "duCorno",
-    senhaUsu : "LevouChifre"
+// const usuario2 = {
+//     nomeUsu : "duCorno",
+//     senhaUsu : "LevouChifre"
 
-}
+// }
 
-//LISTA DE USUÁRIOS
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1, usuario2);
-console.log(listaDeUsuarios);
+// //LISTA DE USUÁRIOS
+// let listaDeUsuarios = [];
+// listaDeUsuarios.push(usuario1, usuario2);
+// console.log(listaDeUsuarios);
 
 
 //console.log(usuario1.nomeUsu);
@@ -78,14 +101,37 @@ addEventListener('click', (evento)=>{
 
     let userInput = document.querySelector("#idUser"); 
     let passInput = document.querySelector("#idPass"); 
+    let eyePass = document.querySelector(".fa");
+
+     // MOSTRAR SENHA
+ 
+    if(evento.target.className == "fa fa-eye" || evento.target.className == "fa fa-eye-slash"){
+       evento.target.setAttribute("style", "cursor: pointer;");       
+
+       if(passInput.getAttribute("type") == "password"){
+            
+            passInput.setAttribute("type", "text");
+            eyePass.setAttribute("class", "fa fa-eye-slash");
+
+       }
+       else{
+            evento.target.setAttribute("style", "cursor: pointer;");   
+            passInput.setAttribute("type", "password");
+            eyePass.setAttribute("class", "fa fa-eye");
+       }
+
+    }
+      // MOSTRAR SENHA
+
+    let listaDeUsuariosRecuperados = JSON.parse(localStorage.getItem("listaUser"));
     
     //console.log(evento.target);
     if(evento.target.id == "btnSubmit"){
 
             try 
             {
-                    listaDeUsuarios.forEach((usuario)=>{  
-                        if(userInput.value == usuario.nomeUsu && passInput.value == usuario.senhaUsu){
+                    listaDeUsuariosRecuperados.forEach((usuario)=>{  
+                        if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
                             throw "foi";
                     
                         }
@@ -98,6 +144,7 @@ addEventListener('click', (evento)=>{
             {
                 if(msg == "foi"){
                     console.log("foi");
+                    window.location.href = "../sucesso.html";
                 }
                 else{
                     console.log("não foi");
